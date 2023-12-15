1bbf338b4bdf0c2fc40bd3bf323ea0e840e0268c
Your Name <you@example.com>
2023-12-15 20:34:28 +0000

    Finishing Authentication Page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,7 +15,7 @@ const Navigation = () => {
                 <Link className="nav-link" to='/shop'>
                     SHOP
                 </Link>
-                <Link className="sign-In" to='/sign-in'>
+                <Link className="nav-link" to='/auth'>
                     SIGN IN
                 </Link>
             </div>
