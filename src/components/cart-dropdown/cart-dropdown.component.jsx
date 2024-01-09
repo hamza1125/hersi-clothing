9b44f4bcae825c8c2f7e5667facb4940707a704a
Your Name <you@example.com>
2024-01-09 09:18:00 +0000

    created checkoutpage along with checkout items and checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,7 @@
 
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Button from '../button/button.component';
 
 import { CartContext } from '../../contexts/cart.context';
@@ -10,12 +12,18 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    };
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items' >
                 {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </div>
     )
 };
