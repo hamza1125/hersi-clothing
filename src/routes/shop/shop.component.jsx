ba1bf65da3deb7fc9e9febfcb0eb7aa5913cddde
Your Name <you@example.com>
2024-03-30 15:37:29 +0000

    implementing redux in categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,32 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+import { setCategories } from '../../store/categories/category.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import './shop.styles.scss'
-
 
 const Shop = () => {
+  const dispatch = useDispatch();
 
-    return (
-        <Routes>
-            <Route index element={<CategoriesPreview/>} />
-            <Route path=':category' index element={<Category/>} />
-        </Routes>
-    );
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categoriesArray));
+    };
+
+    getCategoriesMap();
+  }, [dispatch]);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
 };
 
 export default Shop;
